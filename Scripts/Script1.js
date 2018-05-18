f9b171defe7e1c484576bbb6e6ed1e8b6d7b17d8
Tom <TomEisenbrandt@gmail.com>
2018-05-18 06:17:04 -0500

    Import files
    Importing basics

---
+++ Scripts/Script1.js
@@ -0,0 +1,24 @@
+
+/*
+
+AUTHOR:			Tom Eisenbrandt
+REVISION DATE:	January 4th 2018
+
+*/
+
+
+var myObj, x;
+
+myObj = { "name":"ted", "age":30, "city":"New York" };
+
+x = myObj.name;
+
+document.getElementById("demo").innerHTML = x;
+
+
+/*
+
+END OF FILE
+
+*/
+
